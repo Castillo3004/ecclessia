18ca5116b1bcf96f8035bc78700edfdc2fd6a8fe
Castillo3004 <cacastillo34@utpl.edu.ec>
2023-11-17 20:14:17 -0500

    Header, footer, scrollbar, posible diseños

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -2,10 +2,18 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    HeaderComponent,
+    FooterComponent
+  ],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css'
 })
