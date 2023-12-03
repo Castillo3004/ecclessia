b527cc5fb90f9af8441c61347fa83dc490d27474
Castillo3004 <cacastillo34@utpl.edu.ec>
2023-12-03 11:55:57 -0500

    Se corrigio diseño

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,9 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'primary': '#13283F',
-        'secondary': '#078FA5',
-        'orange-eclessia': '#E67E22',
-        'gris-eclessia': '#34495E',
+        'primary': '#007A7B',
+        'secondary': '#0F2A3D',
+        'yellow-eclessia': '#FDA029',
         'white-eclessia': '#ECF0F1',
         'fondo-transpartente': '#151A1E56',
         '2fondo-transpartente': '#0790A53A',
