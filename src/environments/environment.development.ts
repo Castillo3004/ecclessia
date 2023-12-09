c6916fe0788bdcfda4643396cec1ae4e5daf1f5e
Castillo3004 <cacastillo34@utpl.edu.ec>
2023-12-09 00:38:43 -0500

    Se instalo firebase, firebase-tools, y angular/fire, ademas de generar los environments

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'eclessia-15f44',
+    appId: '1:552316898693:web:524b7e7a88a423075f88ec',
+    storageBucket: 'eclessia-15f44.appspot.com',
+    apiKey: 'AIzaSyCZ6DQrzU9EZ2aE3sJDZoIYCJoTclBZLSE',
+    authDomain: 'eclessia-15f44.firebaseapp.com',
+    messagingSenderId: '552316898693',
+    measurementId: 'G-9SWFX4BE7S',
+  },
+};
