254c544a2afeb7480b0a05ea34fdf61e30f90f74
Castillo3004 <cacastillo34@utpl.edu.ec>
2023-12-09 00:51:22 -0500

    se agrego correctamente el emulador local de firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,9 +3,10 @@ import { provideRouter, withViewTransitions } from '@angular/router';
 
 import { routes } from './app.routes';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { getAuth, provideAuth } from '@angular/fire/auth';
+import { connectAuthEmulator, getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { environment } from '../environments/environment.development';
+import { connectFirestoreEmulator } from 'firebase/firestore';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -17,10 +18,18 @@ export const appConfig: ApplicationConfig = {
       provideFirebaseApp(() => initializeApp( environment.firebase ))
     ),
     importProvidersFrom(
-      provideAuth(() => getAuth())
+      provideAuth(() => {
+        const auth = getAuth();
+        connectAuthEmulator(auth, 'http://localhost:9091', { disableWarnings: true });
+        return auth;
+      }),
     ),
     importProvidersFrom(
-      provideFirestore(() => getFirestore())
+      provideFirestore(() => {
+        const firestore = getFirestore();
+        connectFirestoreEmulator( firestore, 'http://localhost', 9092);
+        return firestore;
+      }),
     ),
   ]
 };
