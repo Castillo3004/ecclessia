dab535c7bcbd9b9b70fa24bca5fd754e0d434770
Castillo3004 <cacastillo34@utpl.edu.ec>
2023-12-09 00:13:27 -0500

    se agrego reactive forms y se conecto con el html

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,29 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 interface LoginIconos{
   urlImg: string;
   name: string;
 }
 
+
 @Component({
   selector: 'auth-login',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, FormsModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export default class LoginComponent {
+export default class LoginComponent{
 
-  public loginIconos: LoginIconos[];
+  // Variables
+  public readonly loginIcons: LoginIconos[] = [
+    { urlImg: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/2021_Facebook_icon.svg/2048px-2021_Facebook_icon.svg.png', name: 'Facebook' },
+    { urlImg: 'https://rotulosmatesanz.com/wp-content/uploads/2017/09/2000px-Google_G_Logo.svg_.png', name: 'Google' },
+    { urlImg: 'https://static-00.iconduck.com/assets.00/linkedin-icon-2048x2048-ya5g47j2.png', name: 'Linkedin' },
+  ]
 
-  constructor(){
-    this.loginIconos = [
-      { urlImg: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/2021_Facebook_icon.svg/2048px-2021_Facebook_icon.svg.png', name: 'Facebook' },
-      { urlImg: 'https://rotulosmatesanz.com/wp-content/uploads/2017/09/2000px-Google_G_Logo.svg_.png', name: 'Google' },
-      { urlImg: 'https://static-00.iconduck.com/assets.00/linkedin-icon-2048x2048-ya5g47j2.png', name: 'Linkedin' },
-    ]
+
+
+  // Inyeccion Dependencias
+  private fb = inject( FormBuilder );
+
+
+
+  public loginForm: FormGroup = this.fb.group({
+    email: ['', [Validators.required, Validators.email] ],
+    password: ['', [Validators.required, Validators.minLength(5)] ],
+    remember: [ false ],
+  });
+
+
+
+
+
+  // Metodos
+
+  ingresar(){
+    console.log(this.loginForm.value);
   }
+
 
 }
--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,14 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'auth-register',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, FormsModule, ReactiveFormsModule ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export default class RegisterComponent {
+
+
+  // Inyeccion Dependencias
+  private fb = inject( FormBuilder );
+
+
+
+  public registerForm: FormGroup = this.fb.group({
+    nombre: ['', [Validators.required, Validators.minLength(5)] ],
+    email: ['', [Validators.required, Validators.email] ],
+    telefono: ['', [Validators.required, Validators.maxLength(10)]],
+    cedula: ['', [Validators.required, Validators.maxLength(10)] ],
+    password: ['', [Validators.required, Validators.minLength(5)] ],
+    password2: ['', [Validators.required, Validators.minLength(5)] ],
+    terminos: [ false, Validators.requiredTrue ],
+  }, {
+    validators: this.passwordsIguales('password', 'password2')
+  });
+
+
+
+
+
+
+
+
+
+
+
+  // Metodos
+
+  registrar(){
+    console.log(this.registerForm.value);
+  }
+
+
+
+
+
+  // Validar Contraseñas Coinciden
+  passwordsIguales(pass1: string, pass2: string, ){
+
+    return ( formGroup: FormGroup) => {
+      const pass1Control = formGroup.get(pass1);
+      const pass2Control = formGroup.get(pass2);
+
+      if( pass1Control?.value === pass2Control?.value ){
+        pass2Control?.setErrors(null);
+      }else{
+        pass2Control?.setErrors( { noEsIgual: true } )
+      }
+    }
+
+  }
+
 
 }
